81806919bbc28becb58f39271c4ce88a188748ab
Ana Rocha <anasoledad.rocha@gmail.com>
2023-02-28 11:04:34 -0300

    Se agregó el ejercicio 11

---
+++ js/ejercicio11.js
@@ -0,0 +1,23 @@
+//11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+
+
+let numeroIngresado = parseInt(prompt("Introduce un número"));
+let divisores = [];
+
+if(numeroIngresado%2===0 || numeroIngresado%3===0 || numeroIngresado%5===0 || numeroIngresado%7===0){
+  if(numeroIngresado%2===0){
+    divisores.push(' '+ 2);
+  }
+  if(numeroIngresado%3===0){
+    divisores.push(' '+ 3);
+  }
+  if(numeroIngresado%5===0){
+    divisores.push(' '+ 5);
+  }
+  if(numeroIngresado%7===0){
+    divisores.push(' '+ 7);;
+  }
+  document.write(`El número ${numeroIngresado} es divisible por:  ${divisores}`);
+}else{
+  document.write(`El número ${numeroIngresado} no es divisible por ninguno de estos números`)
+}
