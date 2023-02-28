99bd52d7814fc660fa87e2a06320f36948c97c6d
Ana Rocha <anasoledad.rocha@gmail.com>
2023-02-28 02:00:35 -0300

    Se modificó el ejercicio 9

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,14 +1,15 @@
 //9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 
-let frase = prompt("Ingrese una frase");
+let frase = prompt('Ingrese una frase:');
 let vocales = [];
 
-for (let i = 0; i < frase.length; i++) {
-  let letra = frase[i].toLowerCase();
-  
-  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
-    vocales.push(' '+letra);
-  }
-}
+frase = frase.toLowerCase();
 
-document.write("Las vocales de la frase son:" + vocales);
+for (let i = 0; i < frase.length; i++) {
+  let letra = frase.charAt(i);
+  if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+    vocales.push(' '+letra);
+  } 
+}
+document.write('Las vocales de la frase son:' + vocales);
+
