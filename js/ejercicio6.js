7915243c8731ca9d5b949cc4a9473a8ba52989c1
Ana Rocha <anasoledad.rocha@gmail.com>
2023-03-01 02:35:31 -0300

    Se agregó validación al ejercicio 6

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,11 +1,20 @@
 //6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
 let numero1 = parseInt(prompt('Ingrese un número'));
-let numero2 = parseInt(prompt('Ingrese otro número'));
 
-if(numero1>numero2){
-    document.write(`El número ${numero1} es mayor que ${numero2}`);
-}else if(numero2>numero1){
-    document.write(`El número ${numero2} es mayor que ${numero1}`);
+if(!isNaN(numero1)){
+    let numero2 = parseInt(prompt('Ingrese otro número'));
+    if(!isNaN(numero2)){
+        if(numero1>numero2){
+            document.write(`El número ${numero1} es mayor que ${numero2}`);
+        }else if(numero2>numero1){
+            document.write(`El número ${numero2} es mayor que ${numero1}`);
+        }else{
+            document.write('Los números son iguales');
+        }
+    }else{
+        document.write(`El valor ingresado no es un número`)
+    }
 }else{
-    document.write('Los números son iguales');
-}
+    document.write(`El valor ingresado no es un número`)
+}   
+
