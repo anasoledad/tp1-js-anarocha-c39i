4190d4b1c04af4273d4ca31f108fc4c7388a5551
Ana Rocha <anasoledad.rocha@gmail.com>
2023-02-28 23:58:15 -0300

    Se agregó validación a ejercicio 11

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -2,20 +2,24 @@
 let numeroIngresado = parseInt(prompt('Introduce un número'));
 let divisores = [];
 
-if(numeroIngresado%2===0 || numeroIngresado%3===0 || numeroIngresado%5===0 || numeroIngresado%7===0){
-  if(numeroIngresado%2===0){
-    divisores.push(' '+ 2);
+if(!isNaN(numeroIngresado)){
+  if(numeroIngresado%2===0 || numeroIngresado%3===0 || numeroIngresado%5===0 || numeroIngresado%7===0){
+    if(numeroIngresado%2===0){
+      divisores.push(' '+ 2);
+    }
+    if(numeroIngresado%3===0){
+      divisores.push(' '+ 3);
+    }
+    if(numeroIngresado%5===0){
+      divisores.push(' '+ 5);
+    }
+    if(numeroIngresado%7===0){
+      divisores.push(' '+ 7);;
+    }
+    document.write(`El número ${numeroIngresado} es divisible por:  ${divisores}`);
+  }else{
+    document.write(`El número ${numeroIngresado} no es divisible por ninguno de estos números`)
   }
-  if(numeroIngresado%3===0){
-    divisores.push(' '+ 3);
-  }
-  if(numeroIngresado%5===0){
-    divisores.push(' '+ 5);
-  }
-  if(numeroIngresado%7===0){
-    divisores.push(' '+ 7);;
-  }
-  document.write(`El número ${numeroIngresado} es divisible por:  ${divisores}`);
 }else{
-  document.write(`El número ${numeroIngresado} no es divisible por ninguno de estos números`)
+    document.write(`El valor ingresado no es un número`)
 }
