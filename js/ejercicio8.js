57722f51b595961c10561fc3024907e0c4f57365
Ana Rocha <anasoledad.rocha@gmail.com>
2023-03-01 01:12:59 -0300

    Se agregó validación al ejercicio 8

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,8 +1,12 @@
 //8.- Escribe un programa que pida un número y diga si es divisible por 2
 let numero1 = parseInt(prompt('Ingrese un número'));
 
-if (numero1 % 2 === 0){
-    document.write(`El número ${numero1} es divisible por 2`);
+if(!isNaN(numero1)){
+    if (numero1 % 2 === 0){
+        document.write(`El número ${numero1} es divisible por 2`);
+    }else{
+        document.write(`El número ${numero1} no es divisible por 2`);
+    }
 }else{
-    document.write(`El número ${numero1} no es divisible por 2`);
+    document.write(`El valor ingresado no es un número`)
 }
