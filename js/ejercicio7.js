86e9525e451a4e376c9d0a34245081ce40b16018
Ana Rocha <anasoledad.rocha@gmail.com>
2023-03-01 02:24:52 -0300

    Se agregó validación al ejercicio 7

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,16 +1,28 @@
 //7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
 let numero1 = parseInt(prompt('Ingrese primer número'));
-let numero2 = parseInt(prompt('Ingrese segundo número'));
-let numero3 = parseInt(prompt('Ingrese tercer número'));
 
-if(numero1!=numero2 || numero1!=numero3 || numero2!=numero3){
-    if(numero1>numero2&&numero1>numero3){
-        document.write(`El número ${numero1} es el mayor`);
-    }else if(numero2>numero3){
-        document.write(`El número ${numero2} es el mayor`);
+if(!isNaN(numero1)){
+    let numero2 = parseInt(prompt('Ingrese segundo número'));
+    if(!isNaN(numero2)){
+        let numero3 = parseInt(prompt('Ingrese tercer número'));
+        if(!isNaN(numero3)){
+            if(numero1!=numero2 || numero1!=numero3 || numero2!=numero3){
+                if(numero1>numero2&&numero1>numero3){
+                    document.write(`El número ${numero1} es el mayor`);
+                }else if(numero2>numero3){
+                    document.write(`El número ${numero2} es el mayor`);
+                }else{
+                    document.write(`El número ${numero3} es el mayor`);
+                }
+            }else{
+                document.write('Los números son iguales');
+            }
+        }else{
+            document.write(`El valor ingresado no es un número`)
+        } 
     }else{
-        document.write(`El número ${numero3} es el mayor`);
-    }
+        document.write(`El valor ingresado no es un número`)
+    }    
 }else{
-    document.write('Los números son iguales');
+    document.write(`El valor ingresado no es un número`)
 }
