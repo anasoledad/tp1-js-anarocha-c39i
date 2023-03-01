7c3ee51408d73edaec13caab7784b671f309a562
Ana Rocha <anasoledad.rocha@gmail.com>
2023-03-01 00:09:52 -0300

    Se modificó el ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,8 +1,20 @@
 //10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
-let numero = parseInt(prompt('Ingrese un número'));
+let numeroIngresado = parseInt(prompt('Ingrese un número'));
 
-if(numero%2===0 || numero%3===0 || numero%5===0 || numero%7===0){
-    document.write('El número ingresado si es divisible al menos por uno de estos números (2, 3, 5, 7)');
-}else{
-    document.write('El número ingresado no es divisible por ninguno de estos números (2, 3, 5, 7)')
-}
+if(!isNaN(numeroIngresado)){
+    if(numeroIngresado%2===0 || numeroIngresado%3===0 || numeroIngresado%5===0 || numeroIngresado%7===0){
+      if(numeroIngresado%2===0){
+        document.write(`El número ${numeroIngresado} es divisible por 2`);
+      }else if(numeroIngresado%3===0){
+        document.write(`El número ${numeroIngresado} es divisible por 3`);
+      }else if(numeroIngresado%5===0){
+        document.write(`El número ${numeroIngresado} es divisible por 5`);
+      }else if(numeroIngresado%7===0){
+        document.write(`El número ${numeroIngresado} es divisible por 7`);
+      }
+    }else{
+      document.write(`El número ${numeroIngresado} no es divisible por 2, 3, 5 ni 7`)
+    }
+  }else{
+      document.write(`El valor ingresado no es un número`)
+  }
